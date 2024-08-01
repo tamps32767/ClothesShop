2a32a699c9ac21b70594ec846aadea3c5736b42c
minhtam711 <tamvmps32767@fpt.edu.vn>
2024-08-01 10:51:53 +0700

    Update Fragment_Home.java

--- app/src/main/java/com/example/clothesshop/Fragment_Home.java
+++ app/src/main/java/com/example/clothesshop/Fragment_Home.java
@@ -3,47 +3,57 @@ package com.example.clothesshop;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.example.clothesshop.adapter.ProductAdapter;
+import com.example.clothesshop.dao.ProductDao;
+import com.example.clothesshop.model.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class Fragment_Home extends Fragment {
 
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private RecyclerView rcvProducts;
+    private ProductAdapter productAdapter;
+    private List<Product> mListProduct;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public Fragment_Home() {
-        // Required empty public constructor
-    }
-
-
-    public static Fragment_Home newInstance(String param1, String param2) {
-        Fragment_Home fragment = new Fragment_Home();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment__home, container, false);
+        View view = inflater.inflate(R.layout.fragment__home, container, false);
+
+        rcvProducts = view.findViewById(R.id.rcvProducts);
+        rcvProducts.setLayoutManager(new LinearLayoutManager(getContext()));
+        mListProduct = new ArrayList<>();
+
+        // Initialize adapter with an empty list
+        productAdapter = new ProductAdapter(new ArrayList<>());
+        rcvProducts.setAdapter(productAdapter);
+
+        // Load product data from API
+        loadProductData();
+
+        return view;
+    }
+
+    private void loadProductData() {
+        mListProduct.add(new Product("Product 1","", "100$", "30","https://example.com/image1.jpg"));
+        mListProduct.add(new Product("Product 2","", "200$", "10","https://example.com/image2.jpg"));
+        mListProduct.add(new Product("Product 3","", "300$", "20","https://example.com/image3.jpg"));
+        mListProduct.add(new Product("Product 4", "","300$","5", "https://example.com/image4.jpg"));
     }
 }
