101dd1ac3a89e2f3a81586d688d520263d8a0ce8
minhtam711 <tamvmps32767@fpt.edu.vn>
2024-08-06 13:15:57 +0700

    Create Cart.java

---
+++ app/src/main/java/com/example/clothesshop/model/Cart.java
@@ -0,0 +1,31 @@
+package com.example.clothesshop.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cart {private static Cart instance;
+    private List<Product> products;
+
+    private Cart() {
+        products = new ArrayList<>();
+    }
+
+    public static synchronized Cart getInstance() {
+        if (instance == null) {
+            instance = new Cart();
+        }
+        return instance;
+    }
+
+    public void addProduct(Product product) {
+        products.add(product);
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void removeProduct(Product product) {
+        products.remove(product);
+    }
+}
